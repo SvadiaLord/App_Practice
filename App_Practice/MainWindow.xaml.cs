b697ee25d9f2d1980ba29e31795601e79e66842e
Vegirskiy_Lord <dmitrijrodikovbvb@gmail.com>
2025-03-31 22:52:02 +0300

    Добавьте файлы проекта.

---
+++ App_Practice/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using App_Practice;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App_Practice
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Practice12_Click(object sender, RoutedEventArgs e)
+        {
+            var work12 = new App_Practice.Practice12Window();
+            work12.Show();
+            this.Hide();
+        }
+
+        private void Practice13_Click(object sender, RoutedEventArgs e)
+        {
+            var work13 = new App_Practice.Practice13Window();
+            work13.Show();
+            this.Hide();
+        }
+
+        private void Practice14_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №14 еще не реализована", "Информация",
+                          MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ App_Practice/Windows/Practice13Window.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace App_Practice.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Practice13Window.xaml
+    /// </summary>
+    public partial class Practice13Window : Window
+    {
+        public Practice13Window()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 30; i++)
+            {
+                TaskSelector.Items.Add(new { Content = $"Задание {i}" });
+            }
+        }
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TaskSelector.SelectedIndex == -1) return;
+
+            int taskNumber = TaskSelector.SelectedIndex + 1;
+            ResultBox.Text = SolveTask(taskNumber);
+        }
+
+        private string SolveTask(int taskNumber)
+        {
+            // Здесь должен быть ваш код для решения задач из практической работы №13
+            return $"Результат выполнения задания {taskNumber}";
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
